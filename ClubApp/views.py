5f6bf5b6a72ff015907e113441d7fa5b19c80541
Dai Nguyen <ngocdai94@gmail.com>
2020-04-25 15:32:53 -0700

    Assignment 5 - Resource View

--- ClubApp/views.py
+++ ClubApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ClubInformation, Meeting, MeetingMinute, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'ClubApp/index.html')
+
+def getresources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'ClubApp/resources.html', {'resource_list':resource_list})
